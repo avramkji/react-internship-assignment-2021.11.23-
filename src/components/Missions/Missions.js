bcc233f41f00ba317775b412a313a5ef58bb9b1f
Avram Kjiprovski <avramkjiprovski@gmail.com>
2022-02-21 23:53:21 +0100

    personal touch

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,49 +1,50 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
 // styles
 import './../../styles/Missions.scss';
-import { Link } from 'react-router-dom';
+
+// MUI Components
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Missions = ({missions}) => {
     return (
-        <Box>
-            <Grid container spacing={4}  className="Missions">
-                <h1 className="title">Missions</h1>
-                
-                    {missions.map(mission => (
-                        <Grid item key={mission.id} xs={12} md={4} >
-                            {console.log("Missions before link: ", mission.rocket.rocket.id)}
-                            <Link to={`/rocket/${mission.rocket.rocket.id}`} style={{textDecoration: 'none'}}>
-                                <Paper>
-                                    <div className="Mission">
-                                        <div className="Paper-head">
-                                            <img 
-                                                src={mission.links.flickr_images[0] || 'https://upload.wikimedia.org/wikipedia/commons/2/22/Falcon_9_close_up.jpeg'} 
-                                                alt={`Rocket for ${mission.mission_name} mission`} 
-                                                className="Mission-image"
-                                            />
-                                        </div>
-                                        
-                                        <div className="Paper-body">
-                                            <h2>{mission.mission_name}</h2>
-                                            <p>
-                                                The {mission.mission_name} mission used the {mission.rocket.rocket.name} rocket and 
-                                                the mission was a {mission.launch_success ? 'success' : 'failure'}.
-                                            </p>
-                                            <p>{mission.launch_date_utc.slice(0, 10)}</p>
-                                        </div>
-                                        
-                                    </div>
-                                </Paper>
-                            </Link>
-                    
-                        </Grid>
-                    ))}
-                
-            </Grid>
-        </Box>
+
+        <div className='Missions'>
+            <h1>Missions</h1>
+            <div className='cardsList'>
+                {missions.map((mission, index) => {
+                    return (
+                    <Card key={mission.id} className="Card" sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={mission.links.flickr_images[0] || 'https://upload.wikimedia.org/wikipedia/commons/2/22/Falcon_9_close_up.jpeg'}
+                            alt={`Rocket for ${mission.mission_name}`}
+                        />
+                        <CardContent className="CardContent">
+                        <Typography gutterBottom variant="h5" component="div">
+                            {mission.mission_name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            The {mission.mission_name} mission used the {mission.rocket.rocket.name} rocket and the mission was a {mission.launch_success ? 'success' : 'failure'}
+                            <br/>
+                            {mission.launch_date_utc.slice(0, 10)}
+                        </Typography>
+                        </CardContent>
+                        <CardActions className="CardActions">
+                        <a href={`/rocket/${mission.rocket.rocket.id}`}>
+                            <Button size="small">Read More!</Button>
+                        </a>
+                        </CardActions>
+                    </Card>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
